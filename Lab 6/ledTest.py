3778f6c5e793d6d5380b265fd396e90a85ee210e
James Donnelly <donnellyfj@gmail.com>
2022-11-06 16:48:29 -0500

    New test

--- Lab 6/ledTest.py
+++ Lab 6/ledTest.py
@@ -1,62 +1,11 @@
 from __future__ import print_function
 import qwiic_led_stick
-import math
 import time
 import sys
 
-def walking_rainbow(LED_stick, rainbow_length, LED_length, delay):
-    red_array = [None] * LED_length
-    blue_array = [None] * LED_length
-    green_array = [None] * LED_length
-
-    for j in range(0, rainbow_length):
-
-        for i in range(0, LED_length):
-            # There are n colors generated for the rainbow
-            # The value of n determins which color is generated at each pixel
-            n = i + 1 - j
-
-            # Loop n so that it is always between 1 and rainbow_length
-            if n <= 0:
-                n = n + rainbow_length
-
-            # The nth color is between red and yellow
-            if n <= math.floor(rainbow_length / 6):
-                red_array[i] = 255
-                green_array[i] = int(math.floor(6 * 255 / rainbow_length * n))
-                blue_array[i] = 0
-            
-            # The nth color is between yellow and green
-            elif n <= math.floor(rainbow_length / 3):
-                red_array[i] = int(math.floor(510 - 6 * 255 / rainbow_length * n))
-                green_array[i] = 255
-                blue_array[i] = 0
-            
-            # The nth color is between green and cyan
-            elif n <= math.floor(rainbow_length / 2):
-                red_array[i] = 0
-                green_array[i] = 255
-                blue_array[i] = int(math.floor(6 * 255 / rainbow_length * n - 510))
-            
-            # The nth color is between blue and magenta
-            elif n <= math.floor(5 * rainbow_length / 6):
-                red_array[i] = int(math.floor(6 * 255 / rainbow_length * n - 1020))
-                green_array[i] = 0
-                blue_array[i] = 255
-            
-            # The nth color is between magenta and red
-            else:
-                red_array[i] = 255
-                green_array[i] = 0
-                blue_array[i] = int(math.floor(1530 - (6 *255 / rainbow_length * n)))
-
-        # Set all the LEDs to the color values accordig to the arrays
-        LED_stick.set_all_LED_unique_color(red_array, green_array, blue_array, LED_length)
-        time.sleep(delay)
-
 def run_example():
 
-    print("\nSparkFun Qwiic LED Stick Example 1")
+    print("\nSparkFun Qwiic LED Stick Example 3")
     my_stick = qwiic_led_stick.QwiicLEDStick()
 
     if my_stick.begin() == False:
@@ -65,12 +14,27 @@ def run_example():
         return
     print("\nLED Stick ready!")
 
+    # Initialize LEDs as a rainbow followed by 1 white pixel
+    red_list = [255, 255, 170, 0, 0, 0, 0, 170, 255, 255]
+    green_list = [0, 170, 255, 255, 255, 170, 0, 0, 0, 255]
+    blue_list = [0, 0, 0, 0, 170, 255, 255, 255, 170, 255]
+
+    # Turn on the LED Stick according to the 3 arrays
+    my_stick.set_all_LED_unique_color(red_list, green_list, blue_list, 10)
+
     while True:
-        walking_rainbow(my_stick, 20, 10, 0.3)
+
+        # This will step through each available brightness setting
+        # Brightness values can be from 0 - 31
+        for i in range(0, 32):
+            my_stick.set_all_LED_brightness(i)
+
+            print("\nBrightness level: " + str(i))
+            time.sleep(1)
 
 if __name__ == '__main__':
     try:
         run_example()
     except (KeyboardInterrupt, SystemExit) as exErr:
-        print("\nEnding Example 8")
+        print("\nEnding Example 4")
         sys.exit(0)
