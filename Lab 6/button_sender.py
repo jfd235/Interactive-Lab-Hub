0ea9c33a0f50fc41a487c4bb9ad9c6454313a2b4
James Donnelly <donnellyfj@gmail.com>
2022-11-06 15:49:06 -0500

    Added button support

---
+++ Lab 6/button_sender.py
@@ -0,0 +1,29 @@
+import time
+import board
+import busio
+import qwiic_button
+
+import paho.mqtt.client as mqtt
+import uuid
+
+client = mqtt.Client(str(uuid.uuid1()))
+client.tls_set()
+client.username_pw_set('idd', 'device@theFarm')
+
+client.connect(
+    'farlab.infosci.cornell.edu',
+    port=8883)
+
+topic = 'IDD/James'
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+button = qwiic_button.QwiicButton()
+
+while True:
+    for i in range(12):
+        if button.is_button_pressed():
+        	val = f"Button touched!"
+        	print(val)
+        	client.publish(topic, val)
+    time.sleep(0.25)
